a75a5ad8256abafe66291a167ecb59667efa9bd8
Kevin Cunningham <kevin@kevincunningham.co.uk>
2020-05-05 17:09:13 +0100

    Revert "Initial state."
    This reverts commit 268a74a2edbe0d226a9ca2957dc67e572bb1a520.

--- pages/api/user.js
+++ pages/api/user.js
@@ -1,26 +1,4 @@
-var MongoClient = require("mongodb").MongoClient;
-var url = "mongodb+srv://kevinc:THISisa1234@course-sm27n.mongodb.net/";
-
 export default async (req, res) => {
-  const user = req.body ? JSON.parse(req.body) : null;
-
-  try {
-    if (!user) {
-      throw "No user object sent";
-    }
-
-    MongoClient.connect(url, function (err, db) {
-      if (err) throw err;
-      var dbo = db.db("test");
-
-      dbo.collection("users").insertOne(user, function (err, response) {
-        if (err) throw err;
-        console.log("1 document inserted");
-        res.send(response);
-      });
-    });
-  } catch (error) {
-    res.status(400);
-    res.send({ error: error });
-  }
+  console.log("got here");
+  res.send({ this: "is a test" });
 };
