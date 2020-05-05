1c661808fd1b339d82e8062ea880d2c613f3e026
Kevin Cunningham <kevin@kevincunningham.co.uk>
2020-05-05 17:48:01 +0100

    Added files.

--- pages/api/user.js
+++ pages/api/user.js
@@ -1,4 +1,26 @@
+var MongoClient = require("mongodb").MongoClient;
+var url = "mongodb+srv://kevinc:THISisa1234@course-sm27n.mongodb.net/";
+
 export default async (req, res) => {
-  console.log("got here");
-  res.send({ this: "is a test" });
+  const user = req.body ? JSON.parse(req.body) : null;
+
+  try {
+    if (!user) {
+      throw "No user object sent";
+    }
+
+    MongoClient.connect(url, function (err, db) {
+      if (err) throw err;
+      var dbo = db.db("test");
+
+      dbo.collection("users").insertOne(user, function (err, response) {
+        if (err) throw err;
+        console.log("1 document inserted");
+        res.send(response);
+      });
+    });
+  } catch (error) {
+    res.status(400);
+    res.send({ error: error });
+  }
 };
